047da654b0893ac2406595ffad9ee31740784a44
baihua <baihua_2011@163.com>
2016-06-22 18:23:46 +0800

    reg

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -6,11 +6,30 @@
  * @author baihua <baihua_2011@163.com>
  */
 class UserController extends BaseController {
-    
+
+    public function actions() {
+        return array(
+            // captcha action renders the CAPTCHA image displayed on the contact page
+            'captcha' => array(
+//				'class' => 'system.web.widgets.captcha.CCaptchaAction',
+                'class' => 'Captcha',
+                'backColor' => 0xE9E5DA,
+                'maxLength' => '4', // 最多生成几个字符
+                'minLength' => '4', // 最少生成几个字符
+                'height' => '40',
+                'testLimit' => 0, //限制相同验证码出现的次数,0为不限制
+            ),
+            'page' => array(
+                'class' => 'CViewAction',
+            ),
+        );
+    }
+
     /**
      * 注册
      */
-    public function actionRegister(){
+    public function actionRegister() {
         $this->render('register');
     }
+
 }
--- web/themes/basic/views/user/register.php
+++ web/themes/basic/views/user/register.php
@@ -1,48 +1,52 @@
+<link href="<?php echo $this->data['static_url']; ?>/autovalidate/style.css" rel="stylesheet">
 <div class="container block_box">
     <div class="text-right"><a href="/"><i class="glyphicon glyphicon-home"></i> 网站首页</a> &nbsp;&nbsp; 已有账号，<a href="<?php echo $this->createAbsoluteUrl('user/login'); ?>">请登录</a></div>
     <ul class="nav nav-tabs" role="tablist">
         <li role="presentation" class="active"><a href="javascript:void(0)">新用户注册</a></li>
     </ul>
     <div class="user_table_box">
-        <form action="" method="" class="user_register">
+        <form action="" method="" class="user_register" id="user_reg_form">
             <table class="table">
                 <tr>
-                    <td>手机号</td>
-                    <td><input class="form-control" type="text" name='mobile' pattern="mobi" alt="填写正确的手机格式" /></td>
-                    <td>填写正确的手机格式</td>
+                    <th class="text-right" style="width: 280px;">手机号</th>
+                    <td><input class="form-control" type="text" name='mobile' pattern="mobi" alt="填写正确的手机格式" /><label>填写正确的手机格式</label></td>
                 </tr>
                 <tr>
-                    <td>登录密码</td>
-                    <td><input class="form-control" type="password" name='password' pattern="^\S{6,32}$" bind='repassword' alt='填写6-32个字符' /></td>
-                    <td>填写登录密码，6-32个字符</td>
+                    <th class="text-right">登录密码</th>
+                    <td><input class="form-control" type="password" name='password' pattern="^\S{6,32}$" bind='repassword' alt='填写6-32个字符' /><label>填写登录密码，6-32个字符</label></td>
                 </tr>
                 <tr>
-                    <td>确认密码</td>
-                    <td><input class="form-control" type="password" name='repassword' pattern="^\S{6,32}$" bind='password' alt='重复上面所填写的密码' /></td>
-                    <td>重复上面所填写的密码</td>
+                    <th class="text-right">确认密码</td>
+                    <td><input class="form-control" type="password" name='repassword' pattern="^\S{6,32}$" bind='password' alt='重复上面所填写的密码' /><label>重复上面所填写的密码</label></td>
                 </tr>
                 <tr>
-                    <td>验证码</td>
-                    <td><input class='form-control' type='text' name='captcha' pattern='^\w{5,10}$' alt='填写图片所示的字符' /></td>
-                    <td>填写图片所示的字符</td>
+                    <th class="text-right"></th>
+                    <td>
+                        <?php $this->widget("CCaptcha", array('showRefreshButton' => true, 'clickableImage' => true, 'buttonType' => 'link', 'buttonLabel' => '换一张', 'imageOptions' => array('alt' => '点击换图', 'title' => '点击换图', 'align' => 'absmiddle'))); ?>
+                    </td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td class="text-center">
+                    <th class="text-right">验证码</th>
+                    <td>
+                        <input class='form-control' type='text' name='captcha' pattern='^\w{5,10}$' alt='填写图片所示的字符' />
+                        <label>填写图片所示的字符</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" class="text-center">
                         <input type="checkbox" name="agreen" checked="true"/>
                         我已阅读并同意
                         <a id="protocol" href="javascript:;">《用户注册协议》</a>
                     </td>
-                    <td></td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td class="text-center">
+                    <td colspan="2" class="text-center">
                         <input type="submit" class="btn btn-lg btn-default" value="立即注册"/>
                     </td>
-                    <td></td>
                 </tr>
             </table>
         </form>
     </div>
 </div>
+<script src="<?php echo $this->data['js_url']; ?>/jquery-1.8.3.min.js"></script>
+<script src="<?php echo $this->data['static_url']; ?>/autovalidate/validate.js"></script>
